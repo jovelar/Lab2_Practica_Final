d6ef9652f9cea6771f7e10fd654cb8a6e2eaea29
Javier Ovelar <isaiasjovelar@gmail.com>
2024-02-13 22:59:17 -0300

    Correciones Practica punteros dobles

--- Practica_Lista_Doble/main.c
+++ Practica_Lista_Doble/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <windows.h>
 
 typedef struct{
     int dato;
@@ -18,7 +18,8 @@ nodo2 *agregarAlFinal(nodo2 *lista, nodo2*nuevo);
 nodo2 *borrarBuscando(nodo2 *lista, int dato);
 nodo2 *borrarAlPrincipio(nodo2 *lista);
 nodo2 *borrarAlFinal(nodo2 *lista);
-void *mostrarLista(nodo2 *lista);
+void mostrarLista(nodo2 *lista);
+void mostrarTodo(nodo2 *lista);
 nodo2 *buscarNodo(nodo2 *lista, int numero);
 void *borrarTodaLaLista(nodo2 *lista);
 
@@ -38,6 +39,8 @@ int main()
             scanf("%d",&numero);
             nodo2 *nuevoNodo=crearNuevoNodo(numero);
             lista=agregarOrdenado(lista,nuevoNodo);
+            printf("\n \t CARGADO! \n");
+            Sleep(800);
             break;
 
         case 50:;//2- AGREGAR DATOS AL PRINCIPIO
@@ -45,6 +48,9 @@ int main()
             printf("\n Ingrese un numero a cargar: ");
             scanf("%d",&numero2);
             nodo2 *nuevoNodo2=crearNuevoNodo(numero2);
+            lista=agregarAlPrincipio(lista,nuevoNodo2);
+            printf("\n Cargado al principio");
+            Sleep(800);
             break;
 
         case 51:;//3- AGREGAR DATOS AL FINAL
@@ -52,6 +58,9 @@ int main()
             printf("\n Ingrese un numero para cargar al final: ");
             scanf("%d",&numero3);
             nodo2 *nuevoNodo3=crearNuevoNodo(numero3);
+            lista=agregarAlFinal(lista,nuevoNodo3);
+            printf("\n Cargado al final");
+            Sleep(800);
             break;
 
         case 52:;//4- BORRAR BUSCANDO UN NODO
@@ -70,7 +79,9 @@ int main()
             break;
 
         case 55://7- MOSTRAR LA LISTA
-            mostrarLista(lista);
+            mostrarTodo(lista);
+            //mostrarLista(lista);
+            system("pause");
             break;
 
         case 56://8- BUSCAR UN NUMERO
@@ -95,6 +106,7 @@ int main()
 
 int menu()
 {
+    system("cls");
     int opcion;
     printf("1- Agregar ordenado a la lista \n");
     printf("2- Agregar datos al principio de la lista \n");
@@ -143,7 +155,7 @@ nodo2 *agregarOrdenado(nodo2 *lista, nodo2 *nuevo)
         {
             nodo2 *iterador=lista;
             nodo2 *anterior=iterador;
-            while(iterador!=NULL && nuevo->dato > iterador->dato)
+            while(iterador!=NULL && nuevo->dato >= iterador->dato)
             {
                 anterior=iterador;
                 iterador=iterador->siguiente;
@@ -169,7 +181,7 @@ nodo2 *agregarOrdenado(nodo2 *lista, nodo2 *nuevo)
 
 nodo2 *agregarAlPrincipio(nodo2 *lista, nodo2 *nuevo)
 {
-    if(!lista)
+    if(!lista) //SI ESTA VACIA !LISTA LO MISMO QUE LISTA==NULL
     {
         lista=nuevo;
     }
@@ -177,6 +189,7 @@ nodo2 *agregarAlPrincipio(nodo2 *lista, nodo2 *nuevo)
     {
         nuevo->siguiente=lista;
         lista->anterior=nuevo;
+        lista=nuevo;
     }
 
     return lista;
@@ -190,7 +203,7 @@ nodo2 *agregarAlFinal(nodo2 *lista, nodo2*nuevo)
     }
     else
     {
-        if(lista->siguiente=NULL)
+        if(lista->siguiente==NULL)
         {
             lista->siguiente=nuevo;
             nuevo->anterior=lista;
@@ -237,7 +250,10 @@ nodo2 *borrarBuscando(nodo2 *lista, int dato)
                 {
                     nodo2 *siguiente=iterador->siguiente;
                     anterior->siguiente=siguiente;
-                    siguiente->anterior=anterior;
+                    if(iterador->siguiente=NULL)
+                    {
+                        siguiente->anterior=anterior;
+                    }
                     free(iterador);
                 }
             }
@@ -247,11 +263,49 @@ nodo2 *borrarBuscando(nodo2 *lista, int dato)
     return lista;
 }
 
-nodo2 *borrarAlPrincipio(nodo2 *lista)
+nodo2 *borrarBuscando2(nodo2 *lista, int dato)
+{
+
+    if(lista)
+    {
+        nodo2 *temp;
+        if(lista->dato==dato)
+        {
+            temp=lista->siguiente;
+            free(lista);
+            lista=temp;
+        }
+        else
+        {
+            nodo2 *iterador=lista;
+            nodo2 *anterior=iterador;
+            while(iterador && dato != iterador->dato)
+            {
+                anterior=iterador;
+                iterador=iterador->siguiente;
+            }
+            if(iterador)
+            {
+                if(dato==iterador->dato)
+                {
+                    nodo2 *siguiente=iterador->siguiente;
+                    anterior->siguiente=siguiente;
+                    if(iterador->siguiente=NULL)
+                    {
+                        siguiente->anterior=anterior;
+                    }
+                    free(iterador);
+                }
+            }
+
+        }
+    }
+}
+nodo2 *borrarAlPrincipio(nodo2 *lista) //VERSION INDEPENDIENTE DE BUSCAR Y BORRAR
 {
     if(lista)
     {
-        if(lista->siguiente=NULL)
+        if(lista->siguiente==NULL)
         {
             free(lista);
             lista=NULL;
@@ -261,37 +315,55 @@ nodo2 *borrarAlPrincipio(nodo2 *lista)
             nodo2 *temp=lista->siguiente;
             free(lista);
             lista=temp;
+            lista->anterior=NULL;
         }
     }
     return lista;
 }
 
-nodo2 *borrarAlFinal(nodo2 *lista)
+nodo2 *borrarAlPrincipio2(nodo2 *lista) //FUNCIONA CON borrarBuscando2();
+{
+    nodo2 *aBorrar=lista;
+    lista=lista->siguiente;
+    free(aBorrar);
+
+    return lista;
+}
+
+nodo2 *borrarAlFinal(nodo2 *lista) //VERSION INDEPENDIENTE DE BUSCAR Y BORRAR
 {
     if(lista)
     {
-        if(lista->siguiente=NULL)
+        if(!lista->siguiente)
         {
-            nodo2 *temp=lista->siguiente;
             free(lista);
-            lista=temp;
+            lista=NULL;
         }
         else
         {
             nodo2 *iterador=lista;
-            nodo2 *anterior=iterador;
-            while(iterador)
+            nodo2 *ante=iterador;
+            while(iterador->siguiente!=NULL)
             {
-                anterior=iterador;
+                ante=iterador;
                 iterador=iterador->siguiente;
             }
-            anterior->siguiente=iterador->siguiente;
             free(iterador);
+            ante->siguiente=NULL;
+
         }
     }
+    return lista;
 }
 
-void *mostrarLista(nodo2 *lista)
+nodo2 *borrarAlFinal2(nodo2 *lista) //FUNCIONA CON borrarBuscando2();
+{
+    nodo2 *anterior=lista->anterior;
+    anterior->siguiente=NULL;
+    free(lista);
+}
+
+void mostrarLista(nodo2 *lista)
 {
     if(lista)
     {
@@ -304,8 +376,35 @@ void *mostrarLista(nodo2 *lista)
             contador++;
         }
     }
+    else
+    {
+        printf("\n La lista esta vacia! \n");
+        Sleep(1000);
+    }
 }
+
+void mostrarTodo(nodo2 *lista)
+{
+    if(lista)
+    {
+        int contador=0;
+        nodo2 *iterador=lista;
+        printf("Mostrando direcciones de memoria y valores \n");
+        printf("|DIR.ANTE|VALOR|DIR.SIG|\n");
+        while(iterador)
+        {
+            printf("|%-8x|%-5i|%-8x|\n",iterador->anterior,iterador->dato,iterador->siguiente);
+            iterador=iterador->siguiente;
+        }
+    }
+    else
+    {
+        printf("\n La lista esta vacia!\n");
+    }
+}
+
 nodo2 *buscarNodo(nodo2 *lista, int numero);
+
 void *borrarTodaLaLista(nodo2 *lista)
 {
     if(lista)
