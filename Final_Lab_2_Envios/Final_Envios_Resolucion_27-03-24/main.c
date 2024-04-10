d6ae50653b286c72cd91c5b4d2e37032b0892982
Javier Ovelar <isaiasjovelar@gmail.com>
2024-04-10 06:48:29 -0300

    .

--- Final_Lab_2_Envios/Final_Envios_Resolucion_27-03-24/main.c
+++ Final_Lab_2_Envios/Final_Envios_Resolucion_27-03-24/main.c
@@ -27,7 +27,23 @@ typedef struct nodoOrigen
    struct nodoOrigen* sig;
 }nodoOrigen;
 
+typedef struct {
+     int cantDestinosAereos;
+     char ciudadOrigen[30];
+     float costoPromedioAereo;
+     float tiempoPromedioAereo;
+   }destinosAereos;
+
+typedef struct {
+     int cantDestinosFerro;;
+     char ciudadOrigen[30];
+     float costoPromedioFerro;
+     float tiempoPromedioFerro;
+   }destinosFerro;
+
 //Punto 1
+
+void pasarACVC(char nombreArchivo[40]);
 
 nodoOrigen *inicOrigen();
 nodoOrigen *crearNodoOrigen(char nombre[30]);
@@ -47,17 +63,31 @@ void mostrarNodosDestino(nodoDestino *lista);
 void mostrarTodo(nodoOrigen *lista);
 
 void ejercicio1(char nombreArchivo[50],nodoOrigen **lista);
-void ejercicio2();
+
+//Punto 2
+
+float promedioRecursivo(nodoOrigen *lista,char origen[50],float sumatoria,int elementos);
+
+//punto 3
+
+void pasarAListasSeparadas(nodoOrigen *lista);
+
+
+void ejercicio2(nodoOrigen *lista);
 void ejercicio3();
 void ejercicio4();
 void ejercicio5();
 
 
+
+
 int main()
 {
     nodoOrigen *lista=NULL;
-    char nombreArchivo[50];
+    char nombreArchivo[50]="registroEnvios.bin";
     ejercicio1(nombreArchivo,&lista);
+    ejercicio2(lista);
+    //pasarACVC("registroEnvios.bin");
     return 0;
 }
 
@@ -169,7 +199,7 @@ nodoOrigen *pasarALDL(char nombreArchivo[40])
                 listaDeLista=agregarOrdenadoRec(listaDeLista,nuevoOrigen);
                 posicionInsertar=nuevoOrigen;
             }
-            if(strcmpi(buffer.origen,"aereo")==0)
+            if(strcmpi(buffer.tipo,"aereo")==0)
             {
                 posicionInsertar->destinosAereos=agregarOrdenado(posicionInsertar->destinosAereos,nuevo);
             }
@@ -240,10 +270,10 @@ void mostrarTodo(nodoOrigen *lista)
         nodoOrigen *iterador=lista;
         while(iterador)
         {
-            printf("ORIGEN: %s\n",iterador->nombre);
-            printf("DESTINOS AEREOS\n");
-            mostrarNDestino(iterador->destinosAereos);
-            printf("DESTINOS FERROCARRIL\n");
+            printf("\nORIGEN: %s\n",iterador->nombre);
+            printf("DESTINOS AEREOS\n\n");
+            mostrarNodosDestino(iterador->destinosAereos);
+            printf("\nDESTINOS FERROCARRIL\n\n");
             mostrarNodosDestino(iterador->destinosFerro);
             iterador=iterador->sig;
         }
@@ -258,4 +288,101 @@ void ejercicio1(char nombreArchivo[50],nodoOrigen **lista)
 {
     *lista=pasarALDL(nombreArchivo);
     mostrarTodo(*lista);
+}
+
+//Punto 2
+
+float promedioRecursivo(nodoOrigen *lista,char origen[50],float sumatoria,int elementos)
+{
+    if(lista)
+    {
+        if(strcmpi(lista->nombre,origen)==0)
+        {
+            nodoDestino *temp=lista->destinosAereos;
+            while(temp)
+            {
+                sumatoria+=temp->costo;
+                elementos++;
+                temp=temp->sig;
+            }
+            sumatoria=sumatoria/elementos;
+        }
+        sumatoria=promedioRecursivo(lista->sig,origen,sumatoria,elementos);
+    }
+    return sumatoria;
+}
+
+float promedioIt(nodoOrigen *lista,char origen[50])
+{
+    float sumatoria=0.0;
+    int contador=0;
+    if(lista)
+    {
+        nodoOrigen *iterador=lista;
+        while(iterador)
+        {
+            if(strcmpi(iterador->nombre,origen)==0)
+            {
+                nodoDestino *it=iterador->destinosAereos;
+                while(it)
+                {
+                    sumatoria+=it->costo;
+                    contador++;
+                    it=it->sig;
+                }
+            }
+            iterador=iterador->sig;
+        }
+
+        if(contador>0)
+        {
+            sumatoria=sumatoria/contador;
+        }
+    }
+
+    return sumatoria;
+}
+
+
+
+void ejercicio2(nodoOrigen *lista)
+{
+    printf("\nIngrese una ciudad correspondiente a un destino aereo: ");
+    char destino[50];
+    gets(destino);
+    //float promedio=promedioIt(lista,destino);
+    float promedio=promedioRecursivo(lista,destino,0.0,0);
+    printf("\nEl promedio es de %0.2f ",promedio);
+
+}
+
+void pasarAListasSeparadas(nodoOrigen *lista)
+{
+    FILE *archivoDAereos=fopen("DestinosAereos.bin","wb");
+    FILE *archivoFerro=fopen("DestinosFerro.bin","wb");
+    nodoOrigen *iteradorO=lista;
+    while(iteradorO)
+    {
+
+    }
+}
+
+void pasarACVC(char nombreArchivo[40])
+{
+    FILE *archivo=fopen(nombreArchivo,"rb");
+    if(archivo)
+    {
+        FILE *archivoSalida=fopen("registros.csv","wb");
+        registroEnvios buffer;
+
+        fprintf(archivoSalida,"ORIGEN;DESTINO;TIPO;TIEMPO;COSTO\n");
+
+        while(fread(&buffer,sizeof(registroEnvios),1,archivo)>0)
+        {
+            fprintf(archivoSalida,"%s;%s;%s;%i;%0.2f\n",buffer.origen,buffer.destino,buffer.tipo,buffer.tiempoViaje,buffer.costo);
+        }
+        fclose(archivoSalida);
+        fclose(archivo);
+
+    }
 }
