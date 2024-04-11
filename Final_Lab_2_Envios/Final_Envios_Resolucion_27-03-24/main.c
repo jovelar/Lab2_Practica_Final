95a777dba80fd34e6a514c4861b883f58a93135c
Javier Ovelar <isaiasjovelar@gmail.com>
2024-04-11 06:36:22 -0300

    .

--- Final_Lab_2_Envios/Final_Envios_Resolucion_27-03-24/main.c
+++ Final_Lab_2_Envios/Final_Envios_Resolucion_27-03-24/main.c
@@ -41,6 +41,11 @@ typedef struct {
      float tiempoPromedioFerro;
    }destinosFerro;
 
+typedef struct
+{
+    int tiempoViaje;
+    struct Pila *siguiente;
+}Pila;
 //Punto 1
 
 void pasarACVC(char nombreArchivo[40]);
@@ -69,12 +74,17 @@ void ejercicio1(char nombreArchivo[50],nodoOrigen **lista);
 float promedioRecursivo(nodoOrigen *lista,char origen[50],float sumatoria,int elementos);
 
 //punto 3
+destinosAereos *nuevoDestAereo(int cantDestinosAereos,char ciudadOrigen[30], float costoPromedioAereo,int tiempoPromedioAereo);
+destinosFerro *nuevoDestFerro(int cantDestinosFerro,char ciudadOrigen[30], float costoPromedioFerro,int tiempoPromedioferro);
+int cuentaDestino(nodoDestino *destinos);
+float costoPromedio(nodoDestino *destinos);
+int tiempoPromedio(nodoDestino *destinos);
 
 void pasarAListasSeparadas(nodoOrigen *lista);
 
 
 void ejercicio2(nodoOrigen *lista);
-void ejercicio3();
+void ejercicio3(nodoOrigen *lista);
 void ejercicio4();
 void ejercicio5();
 
@@ -87,6 +97,7 @@ int main()
     char nombreArchivo[50]="registroEnvios.bin";
     ejercicio1(nombreArchivo,&lista);
     ejercicio2(lista);
+    ejercicio3(lista);
     //pasarACVC("registroEnvios.bin");
     return 0;
 }
@@ -356,6 +367,81 @@ void ejercicio2(nodoOrigen *lista)
 
 }
 
+//punto 3
+destinosAereos *nuevoDestAereo(int cantDestinosAereos,char ciudadOrigen[30], float costoPromedioAereo,int tiempoPromedioAereo)
+{
+    destinosAereos *nuevo=(destinosAereos*)malloc(sizeof(destinosAereos));
+    nuevo->cantDestinosAereos=cantDestinosAereos;
+    strcpy(nuevo->ciudadOrigen,ciudadOrigen);
+    nuevo->costoPromedioAereo=costoPromedioAereo;
+    nuevo->tiempoPromedioAereo=tiempoPromedioAereo;
+
+    return nuevo;
+}
+destinosFerro *nuevoDestFerro(int cantDestinosFerro,char ciudadOrigen[30], float costoPromedioFerro,int tiempoPromedioferro)
+{
+    destinosFerro *nuevo=(destinosFerro*)malloc(sizeof(destinosFerro));
+    nuevo->cantDestinosFerro=cantDestinosFerro;
+    strcpy(nuevo->ciudadOrigen,ciudadOrigen);
+    nuevo->costoPromedioFerro=costoPromedioFerro;
+    nuevo->tiempoPromedioFerro=tiempoPromedioferro;
+
+    return nuevo;
+}
+
+int cuentaDestino(nodoDestino *destinos)
+{
+    int contador=0;
+    nodoDestino *iterador=destinos;
+    while(destinos)
+    {
+        contador++;
+        destinos=destinos->sig;
+    }
+    return contador;
+}
+
+float costoPromedio(nodoDestino *destinos)
+{
+    int contador=0;
+    float sumatoria=0;
+
+    if(destinos)
+    {
+        nodoDestino *iterador=destinos;
+        while(iterador)
+        {
+            sumatoria+=iterador->costo;
+            contador++;
+            iterador=iterador->sig;
+        }
+        sumatoria=sumatoria/contador;
+    }
+
+    return sumatoria;
+}
+
+int tiempoPromedio(nodoDestino *destinos)
+{
+    int sumatoria=0;
+    int contador=0;
+
+    if(destinos)
+    {
+        nodoDestino *iterador=destinos;
+
+        while(iterador)
+        {
+            sumatoria+=iterador->tiempoViaje;
+            contador++;
+            iterador=iterador->sig;
+        }
+        sumatoria=sumatoria/contador;
+    }
+    return sumatoria;
+}
+
+
 void pasarAListasSeparadas(nodoOrigen *lista)
 {
     FILE *archivoDAereos=fopen("DestinosAereos.bin","wb");
@@ -363,10 +449,40 @@ void pasarAListasSeparadas(nodoOrigen *lista)
     nodoOrigen *iteradorO=lista;
     while(iteradorO)
     {
+        nodoDestino *itDestinoAereo=iteradorO->destinosAereos;
+        while(itDestinoAereo)
+        {
+            int cantdestinos=cuentaDestino(itDestinoAereo);
+            int promTiempo=tiempoPromedio(itDestinoAereo);
+            float promCosto=costoPromedio(itDestinoAereo);
+            destinosAereos *nuevo=nuevoDestAereo(cantdestinos,itDestinoAereo->nombre,promCosto,promTiempo);
+            fwrite(nuevo,sizeof(destinosAereos),1,archivoDAereos);
 
+            itDestinoAereo=itDestinoAereo->sig;
+        }
+
+        nodoDestino *itDestinoFerro=iteradorO->destinosFerro;
+        while(itDestinoFerro)
+        {
+            int cantdestinos=cuentaDestino(itDestinoFerro);
+            int promTiempo=tiempoPromedio(itDestinoFerro);
+            float promCosto=costoPromedio(itDestinoFerro);
+            destinosFerro *nuevo=nuevoDestFerro(cantdestinos,itDestinoFerro->nombre,promCosto,promTiempo);
+            fwrite(nuevo,sizeof(destinosFerro),1,archivoFerro);
+            itDestinoFerro=itDestinoFerro->sig;
+        }
+        iteradorO=iteradorO->sig;
     }
+    fclose(archivoFerro);
+    fclose(archivoDAereos);
 }
 
+void ejercicio3(nodoOrigen *lista)
+{
+    pasarAListasSeparadas(lista);
+}
+
+/*
 void pasarACVC(char nombreArchivo[40])
 {
     FILE *archivo=fopen(nombreArchivo,"rb");
@@ -386,3 +502,4 @@ void pasarACVC(char nombreArchivo[40])
 
     }
 }
+*/
