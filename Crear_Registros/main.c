a996fe03a789e2dd09c5b2d3f09d1cf7031ca891
Javier Ovelar <isaiasjovelar@gmail.com>
2024-05-14 07:18:35 -0300

    .

---
+++ Crear_Registros/main.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct
+{
+    int id;
+    char nombreProducto[80];
+    char marca[50];
+    float precio;
+    char tipo[50];
+}Producto;
+
+void crearRegistros(char nombreArchivo[40]);
+
+int main()
+{
+    crearRegistros("RegistroAlimentos.bin");
+}
+
+void crearRegistros(char nombreArchivo[40])
+{
+    FILE *archivo=fopen(nombreArchivo,"wb");
+
+    Producto productos[]={
+        {1,"Yogur Entero 1L","Coto",1199.25,"Lacteo"},
+        {2,"Yogur Entero 160gr","Tregar",1150.00,"Lacteo"},
+        {3,"Yogur Entero con Frutas","Milkaut",1149.00,"Lacteo"},
+        {4,"Yogur Entero con Cereales","Tregar",1100.00,"Lacteo"},
+        {5,"Leche Larga Vida Entera 1L","Coto",949.00,"Lacteo"},
+        {6,"Leche Parc. Descremada 1L","Coto",949.00,"Lacteo"},
+        {7,"Leche entera sachet 1L","Coto",929.00,"Lacteo"},
+        {8,"Leche entera clasica 1L","La Serenisima",1801.90,"Lacteo"},
+        {9,"Leche parcialmente descremada 1L","La Serenisima",1801.90,"Lacteo"},
+        {10,"Queso Untable Mascarpone","Tregar",2300.00,"Lacteo"},
+        {11,"Queso Crema","La Paulina",1125.25,"Lacteo"},
+        {12,"Queso Doble Crema","La Paulina",1287.65,"Lacteo"},
+        {13,"Queso Untable Clasico","Milkaut",1749.00,"Lacteo"},
+        {14,"Queso Gruyere x KG","Veronica",18159.00,"Lacteo"},
+        {15,"Queso Pategras x KG","Veronica",16259.00,"Lacteo"},
+        {16,"Queso Azul x KG","Veronica",18359.00,"Lacteo"},
+        {17,"Queso Port Salud","Veronica",10579.00,"Lacteo"},
+        {18,"Mermelada Ciruela","El Brocal",2304.25,"Conserva"},
+        {19,"Mermelada Durazno","El Brocal",2187.25,"Conserva"},
+        {20,"Dulce de leche","Coto",2308.97,"Conserva"},
+        {21,"Dulce de leche repostero","Coto",1789.00,"Conserva"},
+        {22,"Dulce de leche","Poncho Negro",3587.00,"Conserva"},
+        {23,"Dulce de leche clasico","Milkaut",2540.00,"Conserva"},
+        {24,"Dulce de leche","Ilolay",2380.50,"Conserva"},
+        {25,"Postre vainilla","Exquisita",990.00,"Postre"},
+        {26,"Postre royal","Royal",805.00,"Postre"},
+        {27,"Flan sabor vainilla","Godet",427.00,"Postre"},
+        {28,"Postre dulce de leche","Shimy",1579.50,"Postre"},
+        {29,"Frambuesas con doble chocolate","Gio",4669.00,"Postre"},
+        {30,"Postre helado block","Cofler",4679.35,"Postre"},
+        {31,"Paleta cocida x kg","Paladini",10295.00,"Fiambre"},
+        {32,"Salchicha paladini","Paladini",1515.00,"Fiambre"},
+        {33,"Mortadela mini","Campo Austral",1918.95,"Fiambre"},
+        {34,"Mini salchichon triple","Paladini",2630.00,"Fiambre"},
+        {35,"Bondiola feteada x kg","Lario",3415.00,"Fiambre"},
+        {36,"Fiambre de cerdo feteado x kg","Campo Austral",8449.00,"Fiambre"},
+        {37,"Salame milan","Paladini",3499.00,"Fiambre"},
+        {38,"Salamin tandilero","Cagnoli",15900.00,"Fiambre"},
+        {39,"Coca cola zero 350ml","Coca Cola",1150.00,"Gaseosa"},
+        {40,"Coca cola ligth 2.5L","Coca Cola",2030.00,"Gaseosa"},
+        {41,"Coca cola 500ml","Coca Cola",1150.00,"Gaseosa"},
+        {42,"Coca cola original 2.5L","Coca cola",1809.00,"Gaseosa"},
+        {43,"7 Up 500ml","PepsiCo",893.00,"Gaseosa"},
+        {44,"7 Up 250ml","PepsiCo",496,"Gaseosa"},
+        {45,"Cunnington Cola","Cunnington",1510.00,"Gaseosa"},
+        {46,"Cunnington Pomelo","Cunnington",1510.00,"Gaseosa"},
+        {47,"Schweppers Tonica","Schweppers",2300.00,"Gaseosa"},
+        {48,"Schweppers Zero Pomelo","Schweppers",2300,"Gaseosa"},
+        {49,"Fanta Naranja 2.5L","Coca cola",2750.00,"Gaseosa"},
+        {50,"Jugo Citric Naranja 500ML","Citric",1114.75,"Jugo"},
+        {51,"Jugo Naranja 1L","Puro Sol",1898.90,"Jugo"},
+        {52,"Jugo Manzana 1L","Pura Frutta",2134.11,"Jugo"},
+        {53,"Jugo Pomelo 1L","Puro Sol",1898.90,"Jugo"},
+        {54,"Jugo naranja y durazno 1L","Puro Sol",1898.90,"Jugo"},
+        {55,"Jugo naranja 500ml","Smudis",2095.50,"Jugo"},
+        {56,"Vino Alfredo Coto Cabernet Sauvignon 750ML","Coto",2855.00,"Vino"},
+        {57,"Vino Alfredo Coto Malbec","Coto",2855.00,"Vino"},
+        {58,"Vino Cosecha Tardia 750ML","Benjamin Blanco",2160.00,"Vino"},
+        {59,"Vino Cabernet Savignon 750ML","Fran",2520.00,"Vino"},
+        {60,"Vino Blend Tinto 750ML","Don Nicanor",6420.00,"Vino"},
+        {61,"Vino Chardonnay 750ML","Nieto Senetiner",4080.00,"Vino"},
+        {62,"Vino Malbec 750ML","Trumpeter",16100.00,"Vino"},
+        {63,"Vino Merlot 750ML","Reserva Sophenia",7020.00,"Vino"},
+        {64,"Vino Chardonnay 750ML","Maria Carmen",65300.00,"Vino"}};
+
+
+        fwrite(productos,sizeof(Producto),63,archivo);
+        //pasarACSV(productos,63,"Registros.csv");
+
+
+    fclose(archivo);
+}
--- Practica_Lista2_de_lista_pila_arboles/main.c
+++ Practica_Lista2_de_lista_pila_arboles/main.c
@@ -10,7 +10,7 @@ typedef struct  //Registro
     char marca[50];
     float precio;
     char tipo[50];
-}RegProducto;
+} RegProducto;
 
 typedef struct
 {
@@ -18,13 +18,13 @@ typedef struct
     char nombreProducto[80];
     char marca[50];
     float precio;
-}Producto;
+} Producto;
 
 typedef struct //Lista Simple
 {
     Producto p;
     struct nodoProducto *sig;
-}nodoProducto;
+} nodoProducto;
 
 typedef struct //Lista doble
 {
@@ -32,7 +32,13 @@ typedef struct //Lista doble
     nodoProducto *listaProductos;
     struct nodo2tipo *anterior;
     struct nodo2Tipo *siguiente;
-}nodo2Tipo;
+} nodo2Tipo;
+
+typedef struct
+{
+    float precio;
+    struct Pila *sig;
+}Pila;
 
 Producto crearProducto(int id,char nombreProducto[80],char marca[50],float precio);
 
@@ -41,9 +47,13 @@ nodoProducto *crearNodoProductos(Producto p);
 nodoProducto *insertarProdOrdenado(nodoProducto *lista,nodoProducto *nuevo);
 nodoProducto *insertarProdOrdenadoRec(nodoProducto *lista, nodoProducto *nuevo);
 nodoProducto *buscarProducto(nodoProducto *lista,char nombreProducto[80]);
-int *borrarNodoProducto(nodoProducto *lista,char nombreABorrar[80]);
-void *borraListaProducto(nodoProducto *lista);
+nodoProducto *buscar(nodo2Tipo *lista,char nombreProducto[80]);
+int borrarNodoProducto(nodoProducto **lista,char nombreABorrar[80]);
+int borrar(nodo2Tipo **lista,char nombreProducto[80]);
+nodoProducto *borraListaProducto(nodoProducto *lista);
+nodoProducto *agregarAlFinal(nodoProducto *lista,nodoProducto *nuevo);
 void mostrarProducto(nodoProducto *producto);
+void mostrarListaProducto(nodoProducto *lista);
 
 nodo2Tipo *inicNodo2Tipo();
 nodo2Tipo *crearNodo2Tipo(char tipo[50]);
@@ -54,18 +64,119 @@ nodo2Tipo *borrarTipo(nodo2Tipo *lista,char nombreTipo[50]);
 nodo2Tipo *borrarListaTipo(nodo2Tipo *lista);
 void mostrarListaTipo(nodo2Tipo *lista);
 
+nodo2Tipo *pasarALDDL(nodo2Tipo *lista,char nombreArchivo[80]);
 
+Pila *apilar(Pila *pila,float precio);
+Pila *desapilar(Pila **pila);
+int pilaVacia(Pila *pila);
+float tope(Pila *pila);
+Pila *inicPila();
+Pila *pilaNueva(float precio);
+void mostrarPila(Pila *pila);
 
-
+int menu();
 
 int main()
 {
+    char nombreArchivo[]="RegistroAlimentos.bin";
     int opcion;
     nodo2Tipo *lista=inicNodo2Tipo();
     do
     {
+        opcion=menu();
+        switch(opcion)
+        {
+        case 49:
+        {
+            if(!lista)
+            {
+                lista=pasarALDDL(lista,nombreArchivo);
+                printf("\nLista cargada!\n");
+            }
+            else
+            {
+                printf("\n\nLa lista ya se encuentra cargada!\n\n");
+            }
+        }
+        break;
 
-    }while(opcion!=88 && opcion!=120);
+        case 50:
+            if(lista)
+            {
+                printf("\n\n\n");
+                mostrarListaTipo(lista);
+                printf("\n\n\n");
+            }
+            else
+            {
+                printf("\n\nLa lista esta vacia!\n\n");
+            }
+            break;
+
+        case 51:
+            if(lista)
+            {
+                printf("\nIngrese el producto a buscar: ");
+                char nombreProducto[80];
+                gets(nombreProducto);
+                nodoProducto *resultado=buscar(lista,nombreProducto);
+                if(resultado)
+                {
+                    mostrarProducto(resultado);
+                }
+                else
+                {
+                    printf("\nProducto no encontrado!\n");
+                }
+            }
+            break;
+
+        case 52:
+            if(lista)
+            {
+                printf("\n\n Ingrese el nombre del producto a borrar: ");
+                char productoABorrar[80];
+                gets(productoABorrar);
+                int estado=borrar(&lista,productoABorrar);
+                if(estado==1)
+                {
+                    printf("\nProducto borrado!\n");
+                }
+                else
+                {
+                    printf("\nNo se encontro el producto\n");
+                }
+            }
+            break;
+        case 53:
+            if(lista)
+            {
+                lista=borrarListaTipo(lista);
+            }
+            else
+            {
+                printf("\n\nla lista ya esta vacia!\n\n");
+            }
+            break;
+        case 54:
+            break;
+        case 55:
+            break;
+        case 56:
+            break;
+        case 57:
+            break;
+        case 88:
+            break;
+        case 120:
+            break;
+        default:
+            printf("Opcion invalido!\n");
+            break;
+        }
+
+    }
+    while(opcion!=88 && opcion!=120);
     return 0;
 }
 
@@ -88,18 +199,20 @@ nodoProducto *crearNodoProductos(Producto p)
 {
     nodoProducto *nuevo=(nodoProducto*)malloc(sizeof(nodoProducto));
     nuevo->p=p;
+    nuevo->sig=NULL;
     return nuevo;
 }
 
+
 nodoProducto *insertarProdOrdenado(nodoProducto *lista,nodoProducto *nuevo)
 {
-    if(!lista)
+    if(!lista) //Lista vacia
     {
         lista=nuevo;
     }
     else
     {
-        if(strcmpi(lista->p.nombreProducto,nuevo->p.nombreProducto)>0)
+        if(strcmpi(lista->p.nombreProducto,nuevo->p.nombreProducto)<0) //Primero en la lista
         {
             nuevo->sig=lista;
             lista=nuevo;
@@ -108,18 +221,25 @@ nodoProducto *insertarProdOrdenado(nodoProducto *lista,nodoProducto *nuevo)
         {
             nodoProducto *iterador=lista;
             nodoProducto *ante=lista;
-            while(iterador!=NULL)
+            while(iterador && strcmpi(iterador->p.nombreProducto,nuevo->p.nombreProducto)>0) //Iterar hasta cumplir condicion o fin de lista
             {
-                if(strcmpi(lista->p.nombreProducto,nuevo->p.nombreProducto)>0)
-                {
-                    ante->sig=nuevo;
-                    nuevo->sig=iterador;
-                }
+                ante=iterador;
+                iterador=iterador->sig;
+            }
+            if(!iterador) //Si es el ultimo
+            {
+                ante->sig=nuevo;
+            }
+            else //Si es un nodo intermedio
+            {
+                ante->sig=nuevo;
+                nuevo->sig=iterador;
             }
         }
     }
     return lista;
 }
+
 nodoProducto *insertarProdOrdenadoRec(nodoProducto *lista, nodoProducto *nuevo)
 {
     if(!lista)
@@ -167,22 +287,37 @@ nodoProducto *buscarProducto(nodoProducto *lista,char nombreProducto[80])
     return resultado;
 }
 
-int *borrarNodoProducto(nodoProducto *lista,char nombreABorrar[80])
+nodoProducto *buscar(nodo2Tipo *lista,char nombreProducto[80])
+{
+    nodoProducto *resultado=NULL;
+    if(lista)
+    {
+        nodo2Tipo *iterador=lista;
+        while(iterador && !resultado)
+        {
+            resultado=buscarProducto(iterador->listaProductos,nombreProducto);
+            iterador=iterador->siguiente;
+        }
+    }
+    return resultado;
+}
+
+int borrarNodoProducto(nodoProducto **lista,char nombreABorrar[80])
 {
     int borrado=0;
     if(lista)
     {
-        if(strcmpi(lista->p.nombreProducto,nombreABorrar)==0)
+        if(strcmpi((*lista)->p.nombreProducto,nombreABorrar)==0)
         {
-            nodoProducto *TEMP=lista;
-            free(lista);
-            lista=TEMP;
+            nodoProducto *TEMP=(*lista)->sig;
+            free((*lista));
+            (*lista)=TEMP;
             borrado=1;
         }
         else
         {
-            nodoProducto *iterador=lista;
-            nodoProducto *ante=lista;
+            nodoProducto *iterador=(*lista);
+            nodoProducto *ante=iterador;
             while(iterador && borrado==0)
             {
                 if(strcmpi(iterador->p.nombreProducto,nombreABorrar)==0)
@@ -191,6 +326,7 @@ int *borrarNodoProducto(nodoProducto *lista,char nombreABorrar[80])
                     free(iterador);
                     borrado=1;
                 }
+                ante=iterador;
                 iterador=iterador->sig;
             }
         }
@@ -198,7 +334,25 @@ int *borrarNodoProducto(nodoProducto *lista,char nombreABorrar[80])
     return borrado;
 }
 
-void *borraListaProducto(nodoProducto *lista)
+int borrar(nodo2Tipo **lista,char nombreProducto[80])
+{
+    int estado=0;
+    if(lista)
+    {
+        nodo2Tipo *iterador=(*lista);
+        while(iterador && estado==0)
+        {
+            if(borrarNodoProducto(&iterador->listaProductos,nombreProducto)==1)
+            {
+                estado=1;
+            }
+            iterador=iterador->siguiente;
+        }
+
+    }
+    return estado;
+}
+nodoProducto *borraListaProducto(nodoProducto *lista)
 {
     if(lista)
     {
@@ -210,18 +364,330 @@ void *borraListaProducto(nodoProducto *lista)
             lista=aux;
         }
     }
+    return lista;
 }
 
-void mostrarProducto(nodoProducto *producto);
+nodoProducto *agregarAlFinal(nodoProducto *lista,nodoProducto *nuevo)
+{
+    if(!lista)
+    {
+        lista=nuevo;
+        lista->sig=NULL;
+    }
+    else
+    {
+        nodoProducto *iterador=lista;
+        while(iterador->sig!=NULL)
+        {
+            iterador=iterador->sig;
+        }
+        iterador->sig=nuevo;
+    }
+
+    return lista;
+}
+void mostrarProducto(nodoProducto *producto)
+{
+    printf("%-2i %-30s %-30s %0.2f\n",producto->p.id,producto->p.marca,producto->p.nombreProducto,producto->p.precio);
+}
+
+void mostrarListaProducto(nodoProducto *lista)
+{
+    if(lista)
+    {
+        nodoProducto *iterador=lista;
+        while(iterador)
+        {
+            mostrarProducto(iterador);
+            iterador=iterador->sig;
+        }
+    }
+}
 
 nodo2Tipo *inicNodo2Tipo()
 {
     return NULL;
 }
-nodo2Tipo *crearNodo2Tipo(char tipo[50]);
-nodo2Tipo *agregarOrdenadoN2(nodo2Tipo *lista,nodo2Tipo *nuevo);
-nodo2Tipo *agregarOrdenadoN2Rec(nodo2Tipo *lista,nodo2Tipo *nuevo);
-nodo2Tipo *buscarTipo(nodo2Tipo *lista,char nombreTipo[50]);
-nodo2Tipo *borrarTipo(nodo2Tipo *lista,char nombreTipo[50]);
-nodo2Tipo *borrarListaTipo(nodo2Tipo *lista);
-void mostrarListaTipo(nodo2Tipo *lista);
+
+nodo2Tipo *crearNodo2Tipo(char tipo[50])
+{
+    nodo2Tipo *nuevo=(nodo2Tipo*)malloc(sizeof(nodo2Tipo));
+    strcpy(nuevo->tipo,tipo);
+    nuevo->anterior=NULL;
+    nuevo->listaProductos=NULL;
+    nuevo->siguiente=NULL;
+    return nuevo;
+}
+
+
+nodo2Tipo *agregarOrdenadoN2(nodo2Tipo *lista,nodo2Tipo *nuevo)
+{
+    if(!lista) //Si esta vacio
+    {
+        lista=nuevo;
+    }
+    else
+    {
+        if(strcmpi(lista->tipo,nuevo->tipo)>0) //Si esta en el primer lugar de la lista
+        {
+            nuevo->siguiente=lista;
+            lista->anterior=nuevo;
+            lista=nuevo;
+        }
+        else
+        {
+            nodo2Tipo *iterador=lista;
+            nodo2Tipo *ante=iterador;
+            while(iterador && strcmpi(iterador->tipo,nuevo->tipo)<=0)
+            {
+                ante=iterador;
+                iterador=iterador->siguiente;
+            }
+            if(!iterador)  //Si es el ultimo nodo
+            {
+                ante->siguiente=nuevo;
+                nuevo->anterior=ante;
+            }
+            else  //Si es un nodo intermedio
+            {
+                ante->siguiente=nuevo;
+                nuevo->anterior=ante;
+
+                nuevo->siguiente=iterador;
+                iterador->anterior=nuevo;
+            }
+        }
+    }
+    return lista;
+}
+
+nodo2Tipo *agregarOrdenadoN2Rec(nodo2Tipo *lista,nodo2Tipo *nuevo)
+{
+    if(!lista)
+    {
+        lista=nuevo;
+    }
+    else
+    {
+        if(strcmpi(lista->tipo,nuevo->tipo))
+        {
+            nuevo->anterior=lista->anterior;
+            nuevo->siguiente=lista;
+            lista=nuevo;
+        }
+        else
+        {
+            lista->siguiente=agregarOrdenadoN2Rec(lista->siguiente,nuevo);
+        }
+    }
+
+    return lista;
+}
+
+nodo2Tipo *buscarTipo(nodo2Tipo *lista,char nombreTipo[50])
+{
+    nodo2Tipo *resultado=NULL;
+    if(lista)
+    {
+        if(strcmpi(lista->tipo,nombreTipo)==0)
+        {
+            resultado=lista;
+        }
+        else
+        {
+            nodo2Tipo *iterador=lista;
+            while(iterador && !resultado)
+            {
+                if(strcmpi(iterador->tipo,nombreTipo)==0)
+                {
+                    resultado=iterador;
+                }
+                iterador=iterador->siguiente;
+            }
+        }
+    }
+    return resultado;
+}
+
+nodo2Tipo *borrarTipo(nodo2Tipo *lista,char nombreTipo[50])
+{
+    if(lista)
+    {
+        nodo2Tipo *aBorrar=buscarTipo(lista,nombreTipo);
+        if(aBorrar)
+        {
+            if(!aBorrar->anterior) //Si es el primero
+            {
+                nodo2Tipo *temp=lista->siguiente;
+                free(lista);
+                lista=temp;
+                lista->anterior=NULL;
+            }
+            else
+            {
+                if(!aBorrar->siguiente) //Si es el ultimo
+                {
+                    nodo2Tipo *temp=aBorrar->anterior;
+                    free(aBorrar);
+                    temp->siguiente=NULL;
+                }
+                else  //Si se encuentra entre 2 nodos
+                {
+                    nodo2Tipo *ante=aBorrar->anterior;
+                    nodo2Tipo *sig=aBorrar->siguiente;
+                    free(aBorrar);
+                    ante->siguiente=sig;
+                    sig->anterior=ante;
+                }
+            }
+        }
+    }
+    return lista;
+}
+
+nodo2Tipo *borrarListaTipo(nodo2Tipo *lista)
+{
+    if(lista)
+    {
+        nodo2Tipo *temp;
+        while(lista)
+        {
+            lista->listaProductos=borraListaProducto(lista->listaProductos);
+            temp=lista->siguiente;
+            free(lista);
+            lista=temp;
+        }
+    }
+    return lista;
+}
+
+void mostrarListaTipo(nodo2Tipo *lista)
+{
+    if(lista)
+    {
+        nodo2Tipo *iterador=lista;
+        while(iterador)
+        {
+            printf("\n\n*******%s*******\n\n",iterador->tipo);
+            mostrarListaProducto(iterador->listaProductos);
+            iterador=iterador->siguiente;
+        }
+    }
+}
+
+
+nodo2Tipo *pasarALDDL(nodo2Tipo *lista,char nombreArchivo[80])
+{
+    FILE *archivo=fopen(nombreArchivo,"rb");
+    if(archivo)
+    {
+        RegProducto buffer;
+        while(fread(&buffer,sizeof(RegProducto),1,archivo)>0)
+        {
+            Producto p=crearProducto(buffer.id,buffer.nombreProducto,buffer.marca,buffer.precio);
+            nodoProducto *nuevo=crearNodoProductos(p);
+            nodo2Tipo *posTipo=buscarTipo(lista,buffer.tipo);
+            if(!posTipo)
+            {
+                nodo2Tipo *nuevoT=crearNodo2Tipo(buffer.tipo);
+                posTipo=nuevoT;
+                lista=agregarOrdenadoN2(lista,nuevoT);
+            }
+            posTipo->listaProductos=insertarProdOrdenado(posTipo->listaProductos,nuevo);
+        }
+        fclose(archivo);
+    }
+    else
+    {
+        printf("Archivo no encontrado\n");
+    }
+    return lista;
+}
+
+
+
+Pila *pilaNueva(float precio)
+{
+    Pila *nuevo=(Pila*)malloc(sizeof(Pila));
+    nuevo->precio=precio;
+    nuevo->sig=NULL;
+    return nuevo;
+}
+
+Pila *apilar(Pila *pila,float precio)
+{
+    Pila *nuevo=pilaNueva(precio);
+    if(!pila)
+    {
+        pila=nuevo;
+    }
+    else
+    {
+        nuevo->sig=pila;
+        pila=nuevo;
+    }
+    return pila;
+}
+
+Pila *desapilar(Pila **pila)
+{
+    Pila *retorno=NULL;
+    if(pila)
+    {
+        retorno=pilaNueva((*pila)->precio);
+        Pila *aux=(*pila)->sig;
+        free(*pila);
+        (*pila)=aux;
+    }
+    return retorno;
+}
+
+int pilaVacia(Pila *pila)
+{
+    return(!pila) ? 1: 0;
+}
+
+float tope(Pila *pila)
+{
+    float tope=0;
+    if(pila)
+    {
+        tope=pila->precio;
+    }
+    return tope;
+}
+
+Pila *inicPila()
+{
+    return NULL;
+}
+
+void mostrarPila(Pila *pila)
+{
+    if(pila)
+    {
+        Pila *iterador=pila;
+        while(iterador)
+        {
+            printf("\n\n %0.2f \n\n",iterador->precio);
+            iterador=iterador->sig;
+        }
+    }
+}
+int menu()
+{
+    int opcion=0;
+    printf("1- Pasar registros.bin a Lista Doble de Listas\n");
+    printf("2- Mostrar todo\n");
+    printf("3- Buscar producto\n");
+    printf("4- Borrar producto\n");
+    printf("5- Borrar todas las listas\n");
+    printf("6- Mover a pila los precios menores a X\n");
+    printf("7- Mostrar la pila\n");
+    printf("8- Mover a Arbol todos los productos de marca X\n");
+    printf("9- Mostrar el arbol\n");
+    printf("X- salir \n");
+
+    opcion=getch();
+    return opcion;
+}
