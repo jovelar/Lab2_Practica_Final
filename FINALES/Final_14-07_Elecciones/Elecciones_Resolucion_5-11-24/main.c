38e648d6208c2f62a89087d7ef2f0b4582ba12d7
Javier Ovelar <isaiasjovelar@gmail.com>
2024-11-05 06:44:24 -0300

    .

---
+++ FINALES/Final_14-07_Elecciones/Elecciones_Resolucion_5-11-24/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    char provincia[30];
+    char partido[30];
+    int cant_votos;
+}RecuentoVotos;
+
+
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
